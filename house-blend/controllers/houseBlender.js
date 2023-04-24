faa2a59fa66f0f9e2c1786b131659dd3faf6aaf9
sffw <spencerffwhite@gmail.com>
2023-04-24 16:17:49 -0400

    Add houseblender to controller

---
+++ house-blend/controllers/houseBlender.js
@@ -0,0 +1,25 @@
+/** 
+ *  API DATA NEEDED:
+ * Median housing prices/ zip 
+ * # of coffee shops per zip 
+ * Number of people per zip
+ * 
+ */
+
+/** 
+ *  API DATA NEEDED:
+ * Housblend TM Algo: 
+ *  The houseblend store reflects the relationship between coffee shops, zip code population, and median home prices. 
+ *  coffeeShopPerPerson variable reflects (Zip code population)/(Coffee shops in zip code)
+ *  the houseBlend score reflects (Median house price)/coffeeShopPerPerson 
+ *  As the coffee shops increase, so does the score, making it a desirable place to invest
+ *  As housing prices decrease, 
+ * 
+ */
+
+export default houseBlender(pop, coffeeShops, medPrice){
+
+
+
+
+}
